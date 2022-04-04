8b2ec80cf4a0b4bbf2f780a7dc2e74e5d539af42
Melroy Pereira <melroy.per@gmail.com>
2022-04-04 10:14:44 +0530

    changed guessing number to 63

--- guess_a_number.py
+++ guess_a_number.py
@@ -3,14 +3,14 @@
 for x in range(0,5):
     choice = 4 - x
     inp = int(input("Guess a number:\n"))
-    if inp>62:
+    if inp>63:
         print("Guess lesser number",)
         print("You have only", choice, "choice")
 
-    if inp==62:
+    if inp==63:
         print("congratulation!!! you won")
         break
-    if inp<62:
+    if inp<63:
         print("Guess greater number")
         print("You have only",choice,"choice")
     if choice==0:
